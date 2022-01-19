c53e7e164bb3f3648f181fdd26a7d1135c5f96c1
Mugisha Joyce-Christiane Kakou <mugisha.kakou@gmail.com>
2022-01-19 11:17:42 +0100

    Mongo setup
    - notes schema and types
    - database connection

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,11 +1,13 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
+import dbConnect from '../../src/utils/dbConnect'
 
 type Data = {
   name: string
 }
+dbConnect()
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
---
+++ src/models/Note.ts
@@ -0,0 +1,43 @@
+import mongoose, { Schema, Document } from 'mongoose'
+
+export interface INote extends Document {
+    title: string;
+    description: string;
+    author: string;
+    expiryDate?: number;
+    isPrivate: boolean;
+}
+
+const NoteSchema = new Schema<INote>({
+    title: {
+        type: String,
+        required: [true, "Please add a title to your note"],
+        trim: true,
+        maxlength: [100, "The title cannot be more than 100 characters"]
+    },
+    description: {
+        type: String,
+        required: [true, "Please add some text to your note"],
+        trim: true,
+        maxlength: [3000, "The description cannot be more than 3000 characters"]
+    },
+    author: {
+        type: String,
+        trim: true,
+        required: [true, "Please add the note\'s author"],
+    },
+    expiryDate: {
+        type: Number,
+        default: null
+    },
+    isPrivate: {
+        type: Boolean,
+        default: false
+    }
+
+
+},
+    { timestamps: true })
+
+// if the model exists already, we export it else we create it and export it
+export const Note = mongoose.models.Note || mongoose.model<INote>("Note", NoteSchema);
