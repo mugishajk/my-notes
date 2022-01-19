a607f60e1fd5d1f37b8da93ba026611e41fe9976
Mugisha Joyce-Christiane Kakou <mugisha.kakou@gmail.com>
2022-01-19 11:27:57 +0100

    fixing typescript

--- src/utils/dbConnect.ts
+++ src/utils/dbConnect.ts
@@ -1,15 +1,14 @@
 import mongoose from 'mongoose'
-const connection = { isConnected: null }
+const connection: { isConnected: number; } = {isConnected: NaN} ;
+
+
 
 const dbConnect = async () => {
     if (connection.isConnected) {
         return
     }
 
-    const db = await mongoose.connect(process.env.MONGO_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    })
+    const db = await mongoose.connect(process.env.MONGO_URI || "")
 
     connection.isConnected = db.connections[0].readyState;
     console.log(connection.isConnected)
